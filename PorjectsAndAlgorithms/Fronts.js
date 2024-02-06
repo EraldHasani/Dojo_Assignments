ab168ce5e9f458578bcc50e036e867837431296b
EraldHasani <ero.hasani.99@gmail.com>
2024-02-06 18:56:15 +0100

    projekt

--- PorjectsAndAlgorithms/Fronts.js
+++ PorjectsAndAlgorithms/Fronts.js
@@ -23,17 +23,76 @@ class SLL {
         }
         const removedNode = this.head;
         this.head = this.head.next;
-        return removedNode;
+        return removedNode.data;
+    }
+
+    front() {
+        if (!this.head) {
+            return null; // If the list is empty, return null
+        }
+        return this.head.data;
     }
 }
 
 // Example usage:
 const SLL1 = new SLL();
 SLL1.addFront(18);
-SLL1.addFront(5);
-console.log(SLL1.removeFront()); // Output: Node { data: 5, next: Node { data: 18, next: null } }
-console.log(SLL1.removeFront()); // Output: Node { data: 18, next: null }
-console.log(SLL1.removeFront()); // Output: null (list is empty)
+console.log(SLL1.front());      // Output: 18
+console.log(SLL1.removeFront());// Output: 18
+console.log(SLL1.front());      // Output: null
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  class Node {
+//     constructor(data, next = null) {
+//         this.data = data;
+//         this.next = next;
+//     }
+// }
+
+// class SLL {
+//     constructor() {
+//         this.head = null;
+//     }
+
+//     addFront(value) {
+//         const newNode = new Node(value);
+//         newNode.next = this.head;
+//         this.head = newNode;
+//         return this.head;
+//     }
+
+//     removeFront() {
+//         if (!this.head) {
+//             return null; // If the list is empty, return null
+//         }
+//         const removedNode = this.head;
+//         this.head = this.head.next;
+//         return removedNode;
+//     }
+// }
+
+// // Example usage:
+// const SLL1 = new SLL();
+// SLL1.addFront(18);
+// SLL1.addFront(5);
+// console.log(SLL1.removeFront()); // Output: Node { data: 5, next: Node { data: 18, next: null } }
+// console.log(SLL1.removeFront()); // Output: Node { data: 18, next: null }
+// console.log(SLL1.removeFront()); // Output: null (list is empty)
 
 
 
@@ -85,3 +144,4 @@ console.log(SLL1.removeFront()); // Output: null (list is empty)
 // console.log(SLL1.removeFront()); // Output: Node { data: 5, next: Node { data: 18, next: null } }
 // console.log(SLL1.removeFront()); // Output: Node { data: 18, next: null }
 // console.log(SLL1.removeFront()); // Output: null (list is empty)
+
